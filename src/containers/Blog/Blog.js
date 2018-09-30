f5d721a839101c05d71a74b55b091ce4453bd3d7
John Pawlak <johnpawlak@gmail.com>
2018-09-30 17:12:22 -0400

    new post and add post functionality added

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,22 +8,46 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId : null,
+        error: false,
     }
 
     componentDidMount () {
         axios.get('https://jsonplaceholder.typicode.com/posts')
         .then(response => {
-            this.setState({
-                posts: response.data
+            const posts = response.data.slice(0, 4)
+            const updatedPosts = posts.map(post => {
+                return {
+                    ...post,
+                    author:'Max'
+                }
             })
+            this.setState({
+                posts: updatedPosts
+            })
+        })
+        .catch(err => {
+            this.setState({error: true})
         })
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id})
+    }
+
     render () {
-        const posts = this.state.posts.map((post,index) => {
-            return <Post key={post.id} title={post.title} />
-        })
+        let posts = <p>Something went wrong</p>
+        if(!this.state.error) {
+                posts = this.state.posts.map((post,index) => {
+                    return <Post 
+                    key={post.id} 
+                    title={post.title} 
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)} />
+            })
+        }
+        
 
         return (
             <div>
@@ -31,7 +55,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
